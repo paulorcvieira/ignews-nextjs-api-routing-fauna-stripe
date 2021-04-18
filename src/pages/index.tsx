3af664f34f3a0b5944983a305d3f3e108454720a
Paulo Vieira <paullocostta86@gmail.com>
2021-04-18 18:22:17 -0300

    feat: create document and global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-      <h1 className={styles.title}>
+    <>
+      <Head><title>Home</title></Head>
+      <h1>
         Next.js
       </h1>
+    </>
   )
 }
