8ba188c4e57b6b79d66b156cba46c178f474fc59
Paulo Vieira <paullocostta86@gmail.com>
2021-04-18 21:10:32 -0300

    feat: create home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head'
 
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
     <>
-      <Head><title>Home</title></Head>
-      <h1>
-        Next.js
-      </h1>
+      <Head><title>Home | ig.news</title></Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>For $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
