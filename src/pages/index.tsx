cbb0e9add9b8cebc5b3c2cd48b335ca4fb77eaa8
Paulo Vieira <paullocostta86@gmail.com>
2021-04-20 22:10:19 -0300

    feat: add get static props and subscription button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+
+import { stripe } from '../services/stripe';
+
+import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from '../styles/home.module.scss'
 
-export default function Home() {
+// Client-side Rendering
+// Server-side Rendering (SSR) -> getServerSideProps
+// Static-side Generation (SSG) -> getStaticProps
+
+type HomeProps = {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head><title>Home | ig.news</title></Head>
@@ -13,12 +29,35 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>For $9.90 month</span>
+            <span>For {product.amount} month</span>
           </p>
+
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve(process.env.STRIPE_PRODUCT_PRICE, {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24,
+  }
 }
